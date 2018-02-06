36022f595229c6d0943360dff733be4317a7166f
jasun feddema and Sara Hamilton <jasun@>
2018-02-06 11:18:13 -0800

    create 3 more cshtml pages and fine tune form to collect and display user-entered information correctly.

--- Controllers/CarsController.cs
+++ Controllers/CarsController.cs
@@ -1,34 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using CarDealership.Models;
+using System.Collections.Generic;
 
 namespace CarDealership.Controllers
 {
   public class CarsController : Controller
   {
-    // [HttpGet("/cars")]
-    // public ActionResult Index()
-    // {
-    //     return View();
-    // }
-
-    [HttpGet("/cars/new")]
-    public ActionResult CreateForm()
-    {
-        return View();
-    }
-    [HttpPost("/cars")]
-    public ActionResult Create()
-    {
-      Car newCar = new Car (Request.Form["new-make-model"], Request.Form["new-price"], Request.Form["new-miles"], Request.Form["new-details"]);
-      newCar.Save();
-      return View("Index", newCar);
-    }
 
     [HttpGet("/cars")]
     public ActionResult Index()
     {
         List<Car> allCars = Car.GetAll();
         return View(allCars);
+    }
+
+    [HttpGet("/cars/new")]
+    public ActionResult CreateForm()
+    {
+        return View();
+    }
+
+    [HttpPost("/cars")]
+    public ActionResult Create()
+    {
+      Car newCar = new Car(); newCar.SetMakeModel(Request.Form["new-make-model"]);
+      newCar.SetPrice(int.Parse(Request.Form["new-price"]));
+      newCar.SetMiles(int.Parse(Request.Form["new-miles"]));
+      newCar.SetDetails(Request.Form["new-details"]);
+      newCar.Save();
+      List<Car> allCars = Car.GetAll();
+      return View("Index", allCars);
     }
 
     [HttpPost("/cars/delete")]
--- Models/Car.cs
+++ Models/Car.cs
@@ -1,113 +1,141 @@
 using System;
 using System.Collections.Generic;
 
-class Car
+namespace CarDealership.Models
 {
-  private string _makeModel;
-  private int _price;
-  private int _miles;
-  private string _details;
 
-  public void SetMakeModel(string newMakeModel)
+public class Car
   {
-    _makeModel = newMakeModel;
-  }
-  public string GetMakeModel()
-  {
-    return _makeModel;
-  }
+    private string _makeModel;
+    private int _price;
+    private int _miles;
+    private string _details;
+    private static List<Car> _instances = new List<Car> {};
 
-  public void SetPrice(int newPrice)
-  {
-    if (newPrice >= 0) {
-      _price = newPrice;
-    }
-    else {
-      Console.WriteLine("The price for this item is not valid.");
-    }
-  }
-  public int GetPrice()
-  {
-    return _price;
-  }
+    // public Car (string makeModel, int price, int miles, string details)
+    // {
+    //   _makeModel = makeModel;
+    //   _price = price;
+    //   _miles = miles;
+    //   _details = details;
+    // }
 
-  public void SetMiles(int newMiles)
-  {
-    _miles = newMiles;
-  }
-  public int GetMiles()
-  {
-    return _miles;
-  }
-
-  public void SetDetails(string newDetails)
-  {
-    _details = newDetails;
-  }
-  public string GetDetails()
-  {
-    return _details;
-  }
-
-}
-
-public class Program
-{
-  public static void Main()
-  {
-    Car porsche = new Car();
-    porsche.SetMakeModel("2014 Porsche 911");
-    porsche.SetPrice(114991);
-    porsche.SetMiles(7864);
-    porsche.SetDetails("This car goes fast!");
-
-    Car ford = new Car();
-    ford.SetMakeModel("2011 Ford F450");
-    ford.SetPrice(55995);
-    ford.SetMiles(14241);
-    ford.SetDetails("This car is more affordable.");
-
-    Car lexus = new Car();
-    lexus.SetMakeModel("2013 Lexus RX 350");
-    lexus.SetPrice(44700);
-    lexus.SetMiles(20000);
-    lexus.SetDetails("This car is luxurious.");
-
-    Car mercedes = new Car();
-    mercedes.SetMakeModel("Merceded Benz CLS550");
-    mercedes.SetPrice(39900);
-    mercedes.SetMiles(37979);
-    mercedes.SetDetails("This car is not as expensive as the others.");
-
-    List<Car> Cars = new List<Car>() {porsche, ford, lexus, mercedes};
-
-    foreach(Car automobile in Cars)
+    public void SetMakeModel(string newMakeModel)
     {
-      Console.WriteLine(automobile.GetMakeModel());
+      _makeModel = newMakeModel;
+    }
+    public string GetMakeModel()
+    {
+      return _makeModel;
     }
 
-    Console.WriteLine("Enter maximum price: ");
-    string stringMaxPrice = Console.ReadLine();
-    int maxPrice = int.Parse(stringMaxPrice);
-
-    Console.WriteLine("Enter maximum mileage: ");
-    string stringMaxMileage = Console.ReadLine();
-    int maxMileage = int.Parse(stringMaxMileage);
-
-    List<Car> CarsMatchingSearch = new List<Car>();
-
-    foreach (Car automobile in Cars)
+    public void SetPrice(int newPrice)
     {
-      if (automobile.GetPrice() < maxPrice && automobile.GetMiles() < maxMileage)
-      {
-        CarsMatchingSearch.Add(automobile);
+      if (newPrice >= 0) {
+        _price = newPrice;
+      }
+      else {
+        Console.WriteLine("The price for this item is not valid.");
       }
     }
-    Console.WriteLine("here are the cars that match your search: ");
-    foreach (Car automobile in CarsMatchingSearch)
+    public int GetPrice()
     {
-      Console.WriteLine(automobile.GetMakeModel());
-      Console.WriteLine(automobile.GetDetails());
+      return _price;
     }
+
+    public void SetMiles(int newMiles)
+    {
+      _miles = newMiles;
+    }
+    public int GetMiles()
+    {
+      return _miles;
+    }
+
+    public void SetDetails(string newDetails)
+    {
+      _details = newDetails;
+    }
+    public string GetDetails()
+    {
+      return _details;
+    }
+
+    public static List<Car> GetAll()
+    {
+      return _instances;
+    }
+
+    public void Save()
+    {
+      _instances.Add(this);
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
   }
+
+  // public class Program
+  // {
+  //   public static void Main()
+  //   {
+  //     Car porsche = new Car();
+  //     porsche.SetMakeModel("2014 Porsche 911");
+  //     porsche.SetPrice(114991);
+  //     porsche.SetMiles(7864);
+  //     porsche.SetDetails("This car goes fast!");
+  //
+  //     Car ford = new Car();
+  //     ford.SetMakeModel("2011 Ford F450");
+  //     ford.SetPrice(55995);
+  //     ford.SetMiles(14241);
+  //     ford.SetDetails("This car is more affordable.");
+  //
+  //     Car lexus = new Car();
+  //     lexus.SetMakeModel("2013 Lexus RX 350");
+  //     lexus.SetPrice(44700);
+  //     lexus.SetMiles(20000);
+  //     lexus.SetDetails("This car is luxurious.");
+  //
+  //     Car mercedes = new Car();
+  //     mercedes.SetMakeModel("Merceded Benz CLS550");
+  //     mercedes.SetPrice(39900);
+  //     mercedes.SetMiles(37979);
+  //     mercedes.SetDetails("This car is not as expensive as the others.");
+  //
+  //     List<Car> Cars = new List<Car>() {porsche, ford, lexus, mercedes};
+  //
+  //
+    // foreach(Car automobile in Cars)
+      // {
+      //   Console.WriteLine(automobile.GetMakeModel());
+      // }
+      //
+      // Console.WriteLine("Enter maximum price: ");
+      // string stringMaxPrice = Console.ReadLine();
+      // int maxPrice = int.Parse(stringMaxPrice);
+      //
+      // Console.WriteLine("Enter maximum mileage: ");
+      // string stringMaxMileage = Console.ReadLine();
+      // int maxMileage = int.Parse(stringMaxMileage);
+      //
+      // List<Car> CarsMatchingSearch = new List<Car>();
+      //
+      // foreach (Car automobile in Cars)
+      // {
+      //   if (automobile.GetPrice() < maxPrice && automobile.GetMiles() < maxMileage)
+      //   {
+      //     CarsMatchingSearch.Add(automobile);
+      //   }
+      // }
+      // Console.WriteLine("here are the cars that match your search: ");
+      // foreach (Car automobile in CarsMatchingSearch)
+      // {
+      //   Console.WriteLine(automobile.GetMakeModel());
+      //   Console.WriteLine(automobile.GetDetails());
+      // }
+    // }
 }
